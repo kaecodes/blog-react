95e58dacc336c460bbee02b5699101b10b869d2d
Kaela Nguyen <kaedtn96@gmail.com>
2023-03-04 15:20:36 -0800

    Submit events

--- src/Create.js
+++ src/Create.js
@@ -5,10 +5,17 @@ const Create = () => {
   const [body, setBody] = useState(''); 
   const [author, setAuthor] = useState('mario'); 
 
+  const handleSubmit = (e) => {
+    e.preventDefault(); 
+    const blog = { title, body, author }; 
+
+    console.log(blog); 
+  }
+
   return (
     <div className="create">
       <h2>Add a New Blog</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label>Blog Title:</label>
         <input 
           type="text" 
@@ -35,5 +42,5 @@ const Create = () => {
     </div>
   );
 }
- 
+
 export default Create;
